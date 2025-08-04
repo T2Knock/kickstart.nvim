f4e6e7e38d4e954211bc5a617e09d661fe2b4c28
knock-huynh <huynhtransongngoc@gmail.com>
2025-08-04 11:15:36 +0700

    separate lazy-bootstrap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,7 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>k', 'i<CR><Esc>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
