0e72af506fd8901bf13f26e85d859a302a22f5c4
knock-huynh <huynhtransongngoc@gmail.com>
2025-08-04 18:17:27 +0700

    feat(wk): reconfig wk

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,7 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>k', 'i<CR><Esc>')
+vim.keymap.set('n', '<leader>k', 'i<CR><Esc>', { desc = 'Down a line' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
