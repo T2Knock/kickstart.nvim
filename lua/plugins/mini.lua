48a86371ae27a060512a6527210e9b258727fcbe
knock-huynh <huynhtransongngoc@gmail.com>
2025-08-05 16:19:47 +0700

    refactor(plugins): clean up understand comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -20,19 +20,12 @@ return {
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
       end
 
       require('mini.pairs').setup()
-      require('mini.git').setup()
-      require('mini.diff').setup()
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 }
