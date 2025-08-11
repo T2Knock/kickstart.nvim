8b4b0c929a942a5339790cf5875e97189d9483d7
knock-huynh <huynhtransongngoc@gmail.com>
2025-08-11 15:37:49 +0700

    feat(snippets): understand how to add custom snippets

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true, javascript = true, typescript = true }
+      local disable_filetypes = { c = true, cpp = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
