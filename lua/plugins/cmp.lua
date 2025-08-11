6164090cabc766ee7a6dd8431246b5ae385317d8
knock-huynh <huynhtransongngoc@gmail.com>
2025-08-11 13:20:34 +0700

    feat(cmp): use the native snippet engines instead of lua snip
    not really understand lua snip yet so i'll go with this for now

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,32 +2,12 @@ return {
   'saghen/blink.cmp',
   version = '1.*',
   dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = 'make install_jsregexp',
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
-      opts = {},
-    },
+    'rafamadriz/friendly-snippets',
     'folke/lazydev.nvim',
   },
   opts = {
     keymap = {
       preset = 'default',
-
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
     appearance = {
       nerd_font_variant = 'mono',
@@ -41,7 +21,6 @@ return {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
       },
     },
-    snippets = { preset = 'luasnip' },
     fuzzy = { implementation = 'prefer_rust_with_warning' },
     signature = { enabled = true },
   },
